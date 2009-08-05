28078988e9e23ceabeff6144e395fcb34b9e2a9e
Gilles Dartiguelongue <gilles.dartiguelongue@smartjog.com>
2009-08-05 14:46:05 +0200

    Add scripts for network connection problems, closes: #2229.

---
+++ scripts/ffmpeg_pipe_push.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+TARGET_URL=$(echo -n $(echo $@ | tr ' ' '\n'| tail -n1) )
+
+for arg in $@
+do
+	if [ "$arg" = "$TARGET_URL" ]; then
+		continue
+	fi
+
+	FF_ARGS="${FF_ARGS} $arg"
+done
+
+/usr/bin/ffmpeg ${FF_ARGS} - |  /usr/share/sl2d/push.py 10 ${TARGET_URL}&
+FFPID=`ps | grep '/usr/bin/ffmpeg' | awk '{ print $1 }'`
+wait $!
+kill -9 $FFPID
---
+++ scripts/push.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+
+import fcntl
+import os
+import re
+import select
+import socket
+import sys
+import time
+
+def main():
+    regex = re.match('http://(([^:]+)(:(.*))?@)?([^/@:]+)(:([^/]+))?/(.*)', sys.argv[2])
+    if regex:
+        _, _, _, _, host, _, port, path = regex.groups()
+
+        if not port:
+            port = 21
+    else:
+        print "Malformed URL " + sys.argv[2]
+        sys.exit(1)
+
+    data = 'POST /%(path)s HTTP/1.1\n\
+User-Agent: Lavf52.36.0\n\
+Accept: */*\n\
+Range: bytes=0-\n\
+Host: localhost:8080\n\
+Authorization: Basic\n\
+Connection: close\n\
+\n\
+' % {'path' : path}
+
+    timeout = int(sys.argv[1])
+
+    fcntl.fcntl(0, fcntl.F_SETFL, os.O_NONBLOCK)
+    fcntl.fcntl(1, fcntl.F_SETFL, os.O_NONBLOCK)
+
+    if port is None:
+        port = 80
+    else:
+        port = int(port)
+
+    print host, port, path
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    sock.connect((host, port))
+    sock.send(data)
+    sock.setblocking(False)
+
+    fcntl.fcntl(0, fcntl.F_SETFL, os.O_NONBLOCK)
+    fcntl.fcntl(1, fcntl.F_SETFL, os.O_NONBLOCK)
+
+    last = time.time()
+    max_buf = 10000
+    input_buf = ""
+
+    while True:
+        rlisten = []
+        wlisten = []
+        if len(input_buf) < max_buf:
+            rlisten = [sys.stdin]
+            if len(input_buf) > 0:
+                wlisten = [sock]
+        else:
+            wlisten = [sock]
+
+        fds = select.select(rlisten, wlisten, [], timeout)
+
+        if fds[0]:
+            if len(input_buf) < max_buf:
+                read = sys.stdin.read()
+                if len(read) == 0:
+                    print "STDIN closed. Exiting"
+                    sys.exit(1)
+                input_buf += read
+
+                last = time.time()
+
+        if fds[1]:
+            try:
+                sent = sock.send(input_buf[0:1024])
+                input_buf = input_buf[sent:]
+            except Exception:
+                pass
+
+        if fds == ([], [], []):
+            sys.stderr.write("Pipe timeout (%s seconds)" % timeout)
+            sys.stdin.flush()
+            sys.stdin.close()
+            sys.exit(2)
+
+if __name__ == '__main__':
+    main()
