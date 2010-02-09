82a40eddcbebdc47586fd039d83c0d1f2796c8ff
Gilles Dartiguelongue <gilles.dartiguelongue@smartjog.com>
2010-02-09 12:39:05 +0100

    Adapt scripts to network_timeout option, refs: #2791

--- scripts/ffmpeg_pipe_push.sh
+++ scripts/ffmpeg_pipe_push.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-TARGET_URL=$(echo -n $(echo $@ | tr ' ' '\n'| tail -n1) )
+if [ -z "$FF_TCP_OPTIONS" ]; then
+    export FF_TCP_OPTIONS="timeout=10000"
+fi
 
-for arg in $@
-do
-	if [ "$arg" = "$TARGET_URL" ]; then
-		continue
-	fi
+if [ -z "$FF_UDP_OPTIONS" ]; then
+    export FF_UDP_OPTIONS="timeout=10000"
+fi
 
-	FF_ARGS="${FF_ARGS} $arg"
-done
-
-/usr/bin/ffmpeg ${FF_ARGS} - | /usr/share/sl2d/push.py 10 ${TARGET_URL} &
-FFPID=`ps | grep '/usr/bin/ffmpeg' | awk '{ print $1 }'`
-wait $!
-kill -9 $FFPID >/dev/null 2>&1
+/usr/bin/ffmpeg $@
