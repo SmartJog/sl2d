f3d8b9db13d969293e68d6777270945dda68b574
Gilles Dartiguelongue <gilles.dartiguelongue@smartjog.com>
2010-03-26 16:32:59 +0100

    ffmpeg_pipe_ezstream: add a default network timeout to ffmpeg

--- scripts/ffmpeg_pipe_ezstream.sh
+++ scripts/ffmpeg_pipe_ezstream.sh
@@ -11,6 +11,17 @@ do
 	FF_ARGS="${FF_ARGS} $arg"
 done
 
+# ezstream might die from starving for input but not ffmpeg
+# add timeout in order to avoid zombie creation
+if [ -z "$FF_TCP_OPTIONS" ]; then
+    export FF_TCP_OPTIONS="timeout=10000"
+fi
+
+if [ -z "$FF_UDP_OPTIONS" ]; then
+    export FF_UDP_OPTIONS="timeout=10000"
+fi
+
+
 CONFFILE=/etc/ezstream/$(echo -n $TARGET_URL | md5sum | cut -f1 -d' ')
 
 /usr/bin/ffmpeg ${FF_ARGS} - |  ezstream -c ${CONFFILE} 2> /dev/null
